7abc4f1f7706e3fe1c2844591993691f77a01651
Ahmet Ündemir <ahmetundemir@outlook.com>
2023-12-01 17:33:01 +0300

    ElasticSearch Blog UI Test

---
+++ ElasticSearch.WebUI/Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ElasticSearch.WebUI.Models
+{
+    public class Blog
+    {
+        [JsonPropertyName("_id")]
+        public string Id { get; set; } = null!;
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = null!;
+        [JsonPropertyName("content")]
+        public string Content { get; set; } = null!;
+        [JsonPropertyName("tag")]
+        public string[] Tags { get; set; } = null!;
+        [JsonPropertyName("user_id")]
+        public Guid UserId { get; set; }
+        [JsonPropertyName("created")]
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
